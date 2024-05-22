7f3186bbbaa21f0f44d0a0bce285517e22112c7b
SzilviaTalpai <szilvia.talpai03@e-uvt.ro>
2024-05-22 19:04:36 +0200

    2024-05-22 ST

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from app import app
 from flask import render_template, request, redirect, url_for
+import os
+import json
 import requests
 from bs4 import BeautifulSoup
+from app.utils import extract_content, score, selectors, transformations, translate
 
 @app.route('/')
 def index():
@@ -14,13 +17,35 @@ def extract():
         url =  f"https://www.ceneo.pl/{product_id}"
         response = requests.get(url)
         if response.status_code == requests.codes['ok']:
-            page_dom = BeautifulSoup(response.text, "html.parser")
-            try: 
-                opinions_count = page_dom.select_one('a.product-review__link > span').text.strip()                                              
-            except AttributeError:
-                opinions_count = 0
+            page_dom = BeautifulSoup(response.text, "html.parser") 
+            opinions_count = extract_content(page_dom, 'a.product-review__link > span')                                            
             if opinions_count:
-                # extraction process
+                url =  f"https://www.ceneo.pl/{product_id}#tab=reviews"
+                all_opinions = []
+                while(url):
+                    print(url)
+                    response = requests.get(url)
+                    response.status_code
+                    page_dom = BeautifulSoup(response.text, "html.parser")
+                    opinions = page_dom.select("div.js_product-review") 
+                    for opinion in opinions:
+                        single_opinion = {
+                            key: extract_content(opinion, *value)
+                                for key, value in selectors.items()
+                        }
+                        for key, value in transformations.items():
+                            single_opinion[key] = value(single_opinion[key])
+                        all_opinions.append(single_opinion)
+                    try:
+                        url = "https://www.ceneo.pl"+extract_content(page_dom, "a.pagination__next", "href")
+                    except TypeError:
+                        url = None
+                    if not os.path.exists("app/data"):
+                         os.mkdir("app/data")    
+                    if not os.path.exists("app/data/opinions"):
+                            os.mkdir("app/data/opinions")
+                    with open(f"app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+                            json.dump(all_opinions, jf, indent=4, ensure_ascii=False)   
                 return redirect(url_for('product', product_id=product_id))
             return render_template("extract.html", error = "Product has no opinions.")                            
         return render_template("extract.html", error = "Product does not exist. ")
@@ -28,7 +53,8 @@ def extract():
 
 @app.route('/products')
 def products():
-    return render_template("products.html")
+    products = [filename.split(".")[0] for filename in os.listdir("app/data/opinions")]
+    return render_template("products.html", products=products)
 
 @app.route('/author')
 def author():
@@ -36,4 +62,8 @@ def author():
 
 @app.route('/product/<product_id>')
 def product(product_id):
-    return render_template("product.html", product_id=product_id)
+    if os.path.exists("app/data/opinions"):
+        with open(f"app/data/opinions/{product_id}.json", "r", encoding="UTF-8") as jf:
+            opinions = json.load(jf)  
+        return render_template("product.html", product_id=product_id, opinions = opinions)
+    return redirect(url_for('extract'))
